317aea9baf7d37f0dae6347a9d315f417a802647
Ivan Franjic <2338268+frennky@users.noreply.github.com>
2025-01-23 21:00:03 +0100

    fixed nvim options issue

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,20 +1,20 @@
 require("nvchad.options")
 
-local o = vim.opt
+local o = vim.o
 
 -- Buffer
-o.number = true -- Show line numbers
+o.number = true         -- Show line numbers
 o.relativenumber = true -- Make line numbers relative
-o.wrap = false -- Disable line wrapping
-o.showmode = false -- Don't show mode (already in statusline)
-o.signcolumn = "yes" -- Always show sign column, even when empty
-o.list = true -- Identify whitespace characters
-o.scrolloff = 10 -- Minimum visible lines above and below cursor
-o.listchars = { tab = "» ", trail = "·", nbsp = "␣" } -- Identifiers
-o.fillchars = { eob = " " } -- Remove tildes on empty lines
+o.wrap = false          -- Disable line wrapping
+o.showmode = false      -- Don't show mode (already in statusline)
+o.signcolumn = "yes"    -- Always show sign column, even when empty
+o.list = true           -- Identify whitespace characters
+o.scrolloff = 10        -- Minimum visible lines above and below cursor
+-- o.listchars = { tab = "» ", trail = "·", nbsp = "␣" } -- Identifiers
+-- o.fillchars = { eob = " " } -- Remove tildes on empty lines
 o.expandtab = true
-o.tabstop = 4 -- Convert tab to 4 spaces
-o.shiftwidth = 4 -- Number of spaces for each step of auto-indent
+o.tabstop = 4           -- Convert tab to 4 spaces
+o.shiftwidth = 4        -- Number of spaces for each step of auto-indent
 o.softtabstop = 4
 o.virtualedit = "block" -- Enable virtualedit in block mode
 
@@ -27,9 +27,9 @@ o.termguicolors = true
 
 -- Clipboard and history
 o.clipboard = "unnamedplus" -- Sync OS and Neovim clipboards
-o.undofile = true -- Save undo history
+o.undofile = true           -- Save undo history
 
 -- Search
-o.hlsearch = true -- Highlight on search
+o.hlsearch = true   -- Highlight on search
 o.ignorecase = true -- Case-insensitive search
-o.smartcase = true -- Case-insensitive if \C or capital latters
+o.smartcase = true  -- Case-insensitive if \C or capital latters
